982bc822b772e8f785a0b9ec21f9854f75739166
En1s1 <bestvlog0@gmail.com>
2026-08-04 17:48:30 +0200

    Trigger CodeQL security and quality scan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,8 @@ import Imazhi from "@/assets/images/Imazhi.png";
 
 
 export default function HomePage() {
-  const codeQLSyntaxTest = ;
+  const codeQLSyntaxTest = ;:
+  
   return (
     <div className="w-full">
       {/* Hero Section */}
